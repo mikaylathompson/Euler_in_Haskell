fdec1625932c6f87f0fbbc1ee65344d12edc65d7
mikaylaroset <mikayla.rose.t@gmail.com>
2014-06-30 19:54:51 +0000

    euler1 solved

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,13 @@
 -- | Main entry point to the application.
 module Main where
 
+euler1 :: Integral a => a -> a
+euler1 max = foldl (+) 0 (filter (\x -> (mod x 3 == 0) || (mod x 5 == 0)) [1..max-1])
+
+
+    
 -- | The main entry point.
 main :: IO ()
 main = do
-    putStrLn "Welcome to FP Haskell Center!"
-    putStrLn "Have a good day!"
+    print $ euler1 10
+    print $ euler1 1000
