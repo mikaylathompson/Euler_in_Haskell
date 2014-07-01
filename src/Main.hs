41af8d679a15ea1549b2a0cd874493ed27df2a76
mikaylaroset <mikayla.rose.t@gmail.com>
2014-07-01 13:52:40 +0000

    2 complete

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,18 @@ module Main where
 euler1 :: Integral a => a -> a
 euler1 max = foldl (+) 0 (filter (\x -> (mod x 3 == 0) || (mod x 5 == 0)) [1..max-1])
 
+euler2 :: Int -> Int
+euler2 max = foldl (+) 0 (filter even (fibs_up_to max [1,1]))
+
+--fibs_up_to :: Int -> [Int]->[Int]->[Int] -> Int
+fibs_up_to max xs = if (last xs + last(init xs)) >= max
+                       then xs
+                       else let x0 = last xs
+                                in let x1 = last(init xs)
+                                       in fibs_up_to max (xs ++ [x0 + x1])
 
     
 -- | The main entry point.
 main :: IO ()
 main = do
-    print $ euler1 10
-    print $ euler1 1000
+    print $ euler2 4000000
